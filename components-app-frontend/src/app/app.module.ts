bc88f1921b64741d1aa6481cd9c3f9c532cc3203
Aleksander Wojcik <aleksander.w1992@gmail.com>
2023-10-17 19:35:36 +0200

    generate components

--- components-app-frontend/src/app/app.module.ts
+++ components-app-frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentListComponent } from './app/components/component-list/component-list.component';
+import { ComponentAddComponent } from './app/components/component-add/component-add.component';
+import { ComponentDetailComponent } from './app/components/component-detail/component-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentListComponent,
+    ComponentAddComponent,
+    ComponentDetailComponent
   ],
   imports: [
     BrowserModule,
